6871001e63509c7a6b116264d221f8ecb32b1c37
Saran Tanpituckpong <sarunta@gmail.com>
2017-03-19 23:26:54 +0700

    Fix Travis test fail

--- vbuild.unit.js
+++ vbuild.unit.js
@@ -1,22 +1,16 @@
-const fs = require('fs');
-const path = require('path');
 const karma = require('vbuild-karma');
 
-module.exports = () => {
-	let ghToken = fs.readFileSync(path.resolve(__dirname, './.gh-token'), 'utf8');
-
-	return {
-		// in test we don't need to split vendor code and minimize it
-		vendor: false,
-		minimize: false,
-		sourceMap: false,
-		define: {
-			GH_ROOT_URL: JSON.stringify('https://api.github.com'),
-			GH_TOKEN: JSON.stringify(`?access_token=${ghToken}`)
-		},
-		// run custom build process
-		run(webpackConfig) {
-			karma(webpackConfig);
-		}
-	};
+module.exports = {
+	// in test we don't need to split vendor code and minimize it
+	vendor: false,
+	minimize: false,
+	sourceMap: false,
+	define: {
+		GH_ROOT_URL: JSON.stringify('https://api.github.com'),
+		GH_TOKEN: JSON.stringify('')
+	},
+	// run custom build process
+	run(webpackConfig) {
+		karma(webpackConfig);
+	}
 };
