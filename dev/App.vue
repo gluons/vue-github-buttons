7ad68dbc0aa92b8294ae0a56b63c2180385049f2
Saran Tanpituckpong <sarunta@gmail.com>
2018-07-01 15:54:32 +0700

    :building_construction: Use plain webpack instead of Poi for development

--- dev/App.vue
+++ dev/App.vue
@@ -1,8 +1,20 @@
 <template>
 	<div id="app">
-		<gh-btns-watch slug="vuejs/vue" show-count></gh-btns-watch>
-		<gh-btns-star slug="vuejs/vue" show-count></gh-btns-star>
-		<gh-btns-fork slug="vuejs/vue" show-count></gh-btns-fork>
-		<gh-btns-follow user="yyx990803" show-count></gh-btns-follow>
+		<gh-btns-watch
+			slug="vuejs/vue"
+			show-count
+		/>
+		<gh-btns-star
+			slug="vuejs/vue"
+			show-count
+		/>
+		<gh-btns-fork
+			slug="vuejs/vue"
+			show-count
+		/>
+		<gh-btns-follow
+			user="yyx990803"
+			show-count
+		/>
 	</div>
 </template>
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,69 @@
-const getConfig = require('./build/getConfig');
+const { resolve } = require('path');
+const nvl = require('nvl');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const WebpackBar = require('webpackbar');
+const webpack = require('webpack');
 
-module.exports = [
-	getConfig('web'),
-	getConfig('web', true),
-	getConfig('common'),
-	getConfig('esm')
-];
+const token = nvl(process.env.GITHUB_DEV_TOKEN, '');
+
+const htmlOptions = {
+	title: 'Vue GitHub Buttons',
+	template: resolve(__dirname, './index.pug')
+};
+
+module.exports = {
+	mode: 'development',
+	entry: resolve(__dirname, './dev/index.js'),
+	module: {
+		rules: [
+			{
+				test: /\.vue$/,
+				loader: 'vue-loader'
+			},
+			{
+				test: /\.pug$/,
+				oneOf: [
+					{
+						resourceQuery: /^\?vue/,
+						use: 'pug-plain-loader'
+					},
+					{
+						use: 'pug-loader'
+					}
+				]
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					'vue-style-loader',
+					'css-loader',
+					'sass-loader'
+				]
+			}
+		]
+	},
+	resolve: {
+		extensions: ['.wasm', '.mjs', '.js', '.json', '.vue'],
+		alias: {
+			'@': resolve(__dirname, './src'),
+			'@lib': resolve(__dirname, './lib')
+		}
+	},
+	plugins: [
+		new VueLoaderPlugin(),
+		new HtmlWebpackPlugin(htmlOptions),
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.DefinePlugin({
+			'GH_TOKEN': JSON.stringify(token)
+		}),
+		new WebpackBar()
+	],
+	stats: 'none',
+	devServer: {
+		hot: true,
+		open: true,
+		stats: 'none'
+	},
+	devtool: 'eval-source-map'
+};
